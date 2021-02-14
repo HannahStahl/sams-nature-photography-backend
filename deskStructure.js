7789d3b998a16136ae2613da52413cfbe9d38f1f
Hannah Stahl <hls62@cornell.edu>
2021-02-14 17:40:59 -0600

    Remove Photos from sidebar

--- deskStructure.js
+++ deskStructure.js
@@ -1,19 +1,5 @@
 import S from '@sanity/desk-tool/structure-builder'
 
 export default () =>
-  S.list()
-    .title('Content')
-    .items([
-      S.listItem()
-        .title('Photos')
-        .child(
-          S.documentTypeList('photo')
-          .title('Photos')
-        ),
-      S.listItem()
-        .title('Galleries')
-        .child(
-          S.documentTypeList('gallery')
-          .title('Galleries')
-        )
-    ])
+  S.documentTypeList('gallery')
+    .title('Galleries')
